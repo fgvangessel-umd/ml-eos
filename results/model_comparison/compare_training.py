35075493ed5bdc073581d95279dda272ac01eb69
fgvangessel-umd <fvangess@umd.edu>
2024-07-13 15:08:15 -0400

    Code updates to compare nework training loss.

--- results/model_comparison/compare_training.py
+++ results/model_comparison/compare_training.py
@@ -2,16 +2,17 @@ import os
 import numpy as np
 from matplotlib import pyplot as plt
 
-fig, ax = plt.subplots(figsize=(14,7))
+fig, ax = plt.subplots(figsize=(9,7))
 
 folders = [r'no_constraints/', r'positive/']
 colors = ['navy', 'firebrick']
+network_types = ['unconstrained', 'positive']
 avg = 0
 
 x = np.linspace(0, 249, 250, endpoint=True)
 print(x)
 
-for folder, color in zip(folders, colors):
+for folder, color, network_type in zip(folders, colors, network_types):
     data = np.zeros((10, 250, 3))
     for i, fname in enumerate(os.listdir(folder)):
         dat = np.loadtxt(folder+fname)
@@ -29,12 +30,13 @@ for folder, color in zip(folders, colors):
     ax.plot(x, data[:, :, 1].mean(axis=0), c=color,  linestyle='dotted',linewidth=2.)
     ax.plot(x, data[:, :, 2].mean(axis=0), c=color,  linestyle='dashed',linewidth=2.)
 
-    #ax.fill_between(x, data[:, :, 0].mean(axis=0) - data[:, :, 0].std(axis=0), \
-    #                data[:, :, 0].mean(axis=0) + data[:, :, 0].std(axis=0), \
-    #                alpha=0.5, color=color)
+    ax.fill_between(x, data[:, :, 0].mean(axis=0) - data[:, :, 0].std(axis=0), \
+                    data[:, :, 0].mean(axis=0) + data[:, :, 0].std(axis=0), \
+                    alpha=0.5, color=color, label=network_type)
     
 
 
+ax.legend(fontsize=24)
 ax.set_yscale('log')
 plt.show()
 plt.savefig('training.png')
